11fa2b231cc77db0e8cdacd3de4176189f91e2fa
wglee0511 <ranazang@naver.com>
2024-06-20 11:47:36 +0900

    feature: mapkit test

---
+++ StoryboardPlanetPediaApp/views/MapViewController.swift
@@ -0,0 +1,77 @@
+//
+//  MapViewController.swift
+//  StoryboardPlanetPediaApp
+//
+//  Created by racoon on 6/19/24.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    
+    @objc func mapTapped(_ sender: UITapGestureRecognizer) {
+        let location = sender.location(in: mapView)
+        let coordinate = mapView.convert(location, toCoordinateFrom: mapView)
+        
+        print("Tapped at coordinate: \(coordinate.latitude), \(coordinate.longitude)")
+        
+        // 핀 추가
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = coordinate
+        mapView.removeAnnotations(mapView.annotations)
+        mapView.addAnnotation(annotation)
+        let circle = MKCircle(center: coordinate, radius: 100.0)
+        mapView.addOverlay(circle)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        locationManager.requestWhenInUseAuthorization()
+        
+        mapView.showsUserLocation = true
+        mapView.delegate = self
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(mapTapped(_:)))
+        mapView.addGestureRecognizer(tapGestureRecognizer)
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
+
+extension MapViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, didSelect annotation: MKAnnotation) {
+        print(annotation)
+    }
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        let circleRenderer = MKCircleRenderer(overlay: overlay)
+        circleRenderer.strokeColor = .systemBlue
+        circleRenderer.fillColor = UIColor.systemBlue.withAlphaComponent(0.3)
+        circleRenderer.lineWidth = 1.0
+        return circleRenderer
+    }
+    
+    
+}
+
+extension MapViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print(locations.last ?? "fail")
+    }
+}
